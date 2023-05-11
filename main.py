eb0c3915d95e5507ad2f015dd12033a887acc970
Jonggu Lee <jg.jake.lee@gmail.com>
2023-05-11 17:53:07 +0900

    Init WAS with python

---
+++ main.py
@@ -0,0 +1,53 @@
+from fastapi import FastAPI, Request
+import pymysql
+import uvicorn
+from fastapi.templating import Jinja2Templates
+
+app = FastAPI()
+templates = Jinja2Templates(directory="templates")
+
+@app.middleware("http")
+async def json_content_type_middleware(request: Request, call_next):
+    response = await call_next(request)
+    response.headers["Content-Type"] = "application/json"
+    return response
+
+@app.get("/")
+async def root():
+    return "Hello!"
+
+@app.get("/healthcheck")
+async def healthcheck():
+    return {"status": "OK"}
+
+@app.get("/api/csp")
+async def csp(request: Request):
+    db_connection = DatabaseConnection()
+    data = db_connection.get_all_csp_records()
+    return templates.TemplateResponse("index.html", {"request": request, "data": data})
+
+def main():
+    uvicorn.run(app, host="0.0.0.0", port=3000)
+
+class DatabaseConnection:
+    def __init__(self):
+        self.connection = pymysql.connect(
+            host='localhost',
+            user='cspadmin',
+            password='cspadmin!',
+            database='csp_db',
+            charset='utf8',
+            autocommit=True,
+            cursorclass=pymysql.cursors.DictCursor
+        )
+
+    def get_all_csp_records(self):
+        cur = self.connection.cursor()
+        sql = "SELECT * FROM csp"
+        cur.execute(sql)
+        rows = cur.fetchall()
+        print(rows)
+        return rows
+
+if __name__ == "__main__":
+    main()
---
+++ query.sql
@@ -0,0 +1,23 @@
+CREATE USER 'cspadmin'@'%' IDENTIFIED BY 'cspadmin!';
+GRANT ALL PRIVILEGES ON csp_db.* TO 'cspadmin'@'%';
+CREATE DATABASE csp_db;
+FLUSH PRIVILEGES;
+
+USE csp_db;
+CREATE TABLE IF NOT EXISTS csp (
+  id INT AUTO_INCREMENT PRIMARY KEY,
+  csp_name VARCHAR(100),
+  virtual_servers VARCHAR(100),
+  platform_as_a_service VARCHAR(100),
+  serverless_computing VARCHAR(100),
+  docker_management VARCHAR(100),
+  kubernetes_management VARCHAR(100),
+  object_storage VARCHAR(100),
+  global_content_delivery VARCHAR(100)
+);
+
+INSERT INTO csp (csp_name, virtual_servers, platform_as_a_service, serverless_computing, docker_management, kubernetes_management, object_storage, global_content_delivery)
+VALUES 
+('AWS', 'Instances', 'Elastic Beanstalk', 'Lambda', 'ECS', 'EKS', 'S3', 'CloudFront'),
+('Microsoft Azure', 'VMs', 'Cloud Services', 'Azure Functions', 'Container Service', 'Kubernetes Service', 'Block Blob', 'Delivery Network'),
+('Google Cloud Platform', 'VM Instances', 'App Engine', 'Cloud Functions', 'Container Engine', 'Kubernetes Engine', 'Cloud Storage', 'Cloud CDN');
